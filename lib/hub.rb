06fa56656eeacc727125ccc3755c033ab784a32f
Arvind Kunday <akunday@atlassian.com>
2013-01-18 17:06:29 +1100

    Changing require from github to bitbucket.

--- lib/hub.rb
+++ lib/hub.rb
@@ -1,7 +1,7 @@
 require 'hub/version' unless defined?(Hub::VERSION)
 require 'hub/args'
 require 'hub/ssh_config'
-require 'hub/github_api'
+require 'hub/bitbucket_api'
 require 'hub/context'
 require 'hub/json'
 require 'hub/commands'
